84d759b66fa6724ad1a693f66f02ce3eb03a6117
Fernando Mendes <devfrmendes@gmail.com>
2018-08-07 21:46:13 +0100

    Fix existing packages.
    Why:
    
    * body parser is included in express right now.
    * We are missing helmet.
    
    This change addresses the need by:
    
    * Adding helmet.
    * Replacing body parser with the express default.

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,17 +1,19 @@
 const compression = require("compression");
 const express = require("express");
+const helmet = require("helmet");
 const path = require("path");
 const cors = require("cors");
-const bodyParser = require("body-parser");
 const firefighters = require("./firefighters");
 
 const app = express();
 
-app.use(bodyParser.json());
-
 if (process.env.NODE_ENV !== "production") app.use(cors());
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(helmet());
 app.use(compression());
+
 app.use(
   express.static(path.join(__dirname, "../../dist/"), { maxAge: 86400000 })
 );
