b889d4ab43070315c31e745ddff4af905194620d
Fernando Mendes <devfrmendes@gmail.com>
2018-08-09 11:38:56 +0100

    Add a ping route without basic auth.

--- src/backend/server.js
+++ src/backend/server.js
@@ -22,8 +22,19 @@ if (process.env.NODE_ENV !== "production") {
   app.use(cors());
 } else {
   auth.ensureBasicAuthCredentials();
-  app.use(auth.basicAuth);
+
+  app.all("*", (req, res, next) => {
+    if (req.path !== "/ping") {
+      return auth.basicAuth(req, res, next);
+    }
+
+    return next();
+  });
 }
+
+app.get("/ping", async (req, res) => {
+  res.status(200).send();
+});
 
 app.get("/api/firefighters", async (req, res) => {
   res.send(await firefighters.getFirefighters());
