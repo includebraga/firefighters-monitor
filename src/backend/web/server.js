368f2433cefddfe929a5f68f741e421f5777dcb8
José Sousa <josesousa9000@gmail.com>
2018-08-30 16:34:19 +0100

    HOTFIX: Fix static assets directory

--- src/backend/web/server.js
+++ src/backend/web/server.js
@@ -15,7 +15,7 @@ app.use(helmet());
 app.use(compression());
 
 app.use(
-  express.static(path.join(__dirname, "../../dist/"), { maxAge: 86400000 })
+  express.static(path.join(__dirname, "../../../dist/"), { maxAge: 86400000 })
 );
 
 if (process.env.NODE_ENV !== "production") {
