595d37a470c356b9cfbb031b7f652dae45fe8da2
José Sousa <josesousa9000@gmail.com>
2019-11-07 10:49:03 +0000

    Review

--- src/backend/test/support/factories.js
+++ src/backend/test/support/factories.js
@@ -1,11 +1,13 @@
 const faker = require("faker");
 const { Factory } = require("rosie");
+const { sample } = require("lodash");
 const Firefighter = require("../../models/firefighter");
 
 const firefightersFactory = new Factory().attrs({
   name: faker.name.firstName,
   email: faker.internet.email(),
-  password: "foobar"
+  password: "foobar",
+  status: sample("active", "inactive", "busy")
 });
 
 firefightersFactory.create = (...args) =>
